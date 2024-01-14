94bd19cabd3a9060f4fd6ea0cbab6bd10dd1fb4c
Andrew Monson <andrewmonsondev@gmail.com>
2024-01-13 19:32:16 -0500

    finished tailwind config and tested react router

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -8,6 +8,8 @@ import {
   RouterProvider
 } from 'react-router-dom'
 import HomeScreen from './screens/HomeScreen.jsx'
+import './index.css'
+import { ThemeProvider } from '@material-tailwind/react'
 
 
 const router = createBrowserRouter(
@@ -20,6 +22,8 @@ const router = createBrowserRouter(
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router}/>
+    <ThemeProvider>
+      <RouterProvider router={router}/>
+    </ThemeProvider>
   </React.StrictMode>,
 )
