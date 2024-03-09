528c13d648bcd30f36933ac41d337714b62b03b7
Andrew Monson <139920681+AndrewCMonson@users.noreply.github.com>
2024-03-09 11:07:20 -0500

    updated seeder to add stock images to all schools

--- backend/data/images.js
+++ backend/data/images.js
@@ -2,32 +2,26 @@ const images = [
 	{
 		url: 'https://via.placeholder.com/150',
 		alt: 'school 1',
-		owner: '',
 	},
 	{
 		url: 'https://via.placeholder.com/150',
 		alt: 'school 2',
-		owner: '',
 	},
 	{
 		url: 'https://via.placeholder.com/150',
 		alt: 'school 3',
-		owner: '',
 	},
 	{
 		url: 'https://via.placeholder.com/150',
 		alt: 'school 4',
-		owner: '',
 	},
 	{
 		url: 'https://via.placeholder.com/150',
 		alt: 'school 5',
-		owner: '',
 	},
 	{
 		url: 'https://via.placeholder.com/150',
 		alt: 'school 6',
-		owner: '',
 	},
 ];
 
--- backend/seeder.js
+++ backend/seeder.js
@@ -1,9 +1,9 @@
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import schools from './data/schools.js';
-import School from './models/SchoolsModel.js';
+import { School } from './models/SchoolsModel.js';
 import images from './data/images.js';
-import User from './models/UserModel.js';
+import { User } from './models/UserModel.js';
 import users from './data/user.js';
 
 dotenv.config();
@@ -12,50 +12,37 @@ import connectDB from './config/db.js';
 connectDB();
 
 const importData = async () => {
-    try {
-        await School.deleteMany();
-        await User.deleteMany();
+	try {
+		await School.deleteMany();
+		await User.deleteMany();
 
-        
+		const createdSchools = await School.insertMany(schools);
 
-        const createdSchools = await School.insertMany(schools);
+		await User.insertMany(users);
 
-        const imagesWithOwner = images.map(image => {
-            return { ...image, owner: createdSchools[0]._id };
-        });
-
-        const updatedSchools = await School.findByIdAndUpdate(
-            createdSchools[0]._id,
-            { images: imagesWithOwner },
-            { new: true }
-        );
-
-
-        await User.insertMany(users);
-
-        console.log('Data imported');
-        process.exit();
-    } catch (error) {
-        console.error(error);
-        process.exit(1);
-    }
+		console.log('Data imported');
+		process.exit();
+	} catch (error) {
+		console.error(error);
+		process.exit(1);
+	}
 };
 
 const destroyData = async () => {
-    try {
-        await School.deleteMany();
-        await User.deleteMany();
+	try {
+		await School.deleteMany();
+		await User.deleteMany();
 
-        console.log('Data destroyed');
-        process.exit();
-    } catch (error) {
-        console.error(error);
-        process.exit(1);
-    }
+		console.log('Data destroyed');
+		process.exit();
+	} catch (error) {
+		console.error(error);
+		process.exit(1);
+	}
 };
 
 if (process.argv[2] === '-d') {
-    destroyData();
+	destroyData();
 } else {
-    importData();
+	importData();
 }
