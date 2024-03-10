555f368b47057cb798abf32021c2bf7d43c91e99
Andrew Monson <andrewmonsondev@gmail.com>
2024-03-09 21:01:25 -0500

    implemented google reverse geocoding api for using client location for search

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,65 +1,94 @@
-import {
-	Button,
-	Input,
-} from '@material-tailwind/react';
+import { Button, Input } from '@material-tailwind/react';
 import { useNavigate } from 'react-router-dom';
 
 export const HomeScreen = () => {
-
 	const navigate = useNavigate();
 
 	const handleSearchSubmit = event => {
 		event.preventDefault();
 
 		navigate(`/schools?zipcode=${event.target.zipcode.value}`);
-	}
+	};
+
+	const handleUseLocationClick = () => {
+		if (navigator.geolocation) {
+			navigator.geolocation.getCurrentPosition(position => {
+				const { latitude, longitude } = position.coords;
+				fetch(
+					`https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=${
+						import.meta.env.VITE_GOOGLE_MAPS_API_KEY
+					}`
+				)
+					.then(response => response.json())
+					.then(data => {
+						const zipcode = data.results[0].address_components.find(component =>
+							component.types.includes('postal_code')
+						).long_name;
+
+						navigate(`/schools?zipcode=${zipcode}`);
+					});
+			});
+		} else {
+			navigate('/schools');
+		}
+	};
 
 	return (
 		<>
-			<section
-				id="homeScreen"
-				className="flex justify-end h-full w-100"
-			>
-				<div className="flex flex-col bg-white justify-center p-8 ">
+			<section id="homeScreen" className="flex justify-end h-full w-100">
+				<div className="flex flex-col bg-white p-8 ">
 					<div className="container mx-auto">
 						<h1 className="text-center text-4xl mb-4">
-							Welcome to Your Schools
+							Welcome to{' '}
+							<span className="text-indigo-800 font-bold italic">
+								Your Schools
+							</span>
 						</h1>
 						<p className="text-center text-2xl">
-							Find the best pre-schools and daycares for your child
+							<span className="font-bold">High quality</span>, trusted
+							preschools and daycares in your area
 						</p>
 					</div>
+
 					<div className="container mx-auto flex flex-row justify-center mt-4">
-						
-							<form
-								onSubmit={handleSearchSubmit}
-								className="container mx-auto relative flex w-full max-w-[24rem]"
+						<form
+							onSubmit={handleSearchSubmit}
+							className="container mx-auto relative flex w-full max-w-[24rem]"
+							label="Search"
+						>
+							<Input
+								type="text"
+								name="zipcode"
+								label="Enter Your Zipcode To Start!"
+								className="pr-20"
+								maxLength={5}
+								containerProps={{ className: 'min-w-0' }}
+							/>
+							<Button
+								type="submit"
+								size="sm"
+								color="indigo"
+								className="!absolute right-1 top-1 rounded"
 								label="Search"
 							>
-								<Input
-									type="text"
-									name="zipcode"
-									label="Enter Your Zipcode To Start!"
-									className="pr-20"
-									
-									maxLength={5}
-									containerProps={{ className: 'min-w-0' }}
-								/>
-								<Button
-									type="submit"
-									size="sm"
-									color="indigo"
-									className="!absolute right-1 top-1 rounded"
-									label="Search"
-								>
-									Search
-								</Button>
-							</form>
-						
+								Search
+							</Button>
+						</form>
+					</div>
+					<div>
+						<div className="container mx-auto flex flex-row justify-center mt-4">
+							<p className="text-center text-2xl">
+								<span className="font-bold">OR</span>
+							</p>
+						</div>
+						<div className="container mx-auto flex flex-row justify-center mt-4">
+							<Button size="lg" color="indigo" onClick={handleUseLocationClick}>
+								Use Your Location
+							</Button>
+						</div>
 					</div>
 				</div>
-				
 			</section>
 		</>
 	);
-}
+};
