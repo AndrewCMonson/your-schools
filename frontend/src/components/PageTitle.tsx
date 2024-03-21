9f2672451a9274374157b7d3995522997e67b0b1
Andrew Monson <andrewmonsondev@gmail.com>
2024-03-21 11:29:44 -0400

    page title component converted to typescript

--- frontend/src/components/PageTitle.tsx
+++ frontend/src/components/PageTitle.tsx
@@ -1,4 +1,8 @@
-const PageTitle = ({ title }) => {
+interface PageTitleProps {
+  title: string;
+}
+
+const PageTitle = ({ title }: PageTitleProps) => {
   return (
     <div className="container mx-auto my-4">
       <h1 className="text-center text-4xl">{title}</h1>
