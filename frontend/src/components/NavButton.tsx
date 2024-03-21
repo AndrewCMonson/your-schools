baf51b9875b784535169f4bdb8b9404c2b45b245
Andrew Monson <andrewmonsondev@gmail.com>
2024-03-21 14:42:23 -0400

    converted NavButton to TS

--- frontend/src/components/NavButton.tsx
+++ frontend/src/components/NavButton.tsx
@@ -1,12 +1,17 @@
 import { Link } from "react-router-dom";
+import { HTMLAttributes } from "react";
 
-const NavButton = ({ name, link, onClick }) => {
+interface NavButtonProps extends Pick<HTMLAttributes<HTMLDivElement>, "onClick"> {
+  name: string;
+  link: string;
+}
+
+const NavButton = ({ name, link }: NavButtonProps) => {
   return (
     <div className="flex items-center">
       <Link
         to={link}
         className="text-gray-900 hover:bg-indigo-800 hover:text-white px-3 py-2 rounded-md text-sm font-medium 3xl:text-xl"
-        onClick={onClick}
       >
         {name}
       </Link>
