4d406821777fc92429523d8217e6ff2c8979815c
Andrew Monson <andrewmonsondev@gmail.com>
2024-03-23 15:10:38 -0400

    created dedicated test folder for unit testing

--- frontend/src/tests/components/Footer.test.tsx
+++ frontend/src/tests/components/Footer.test.tsx
@@ -1,6 +1,6 @@
 import { describe, expect, test } from 'vitest';
 import { render, screen } from '@testing-library/react';
-import { Footer } from './Footer';
+import { Footer } from '../../components';
 
 describe('Footer', () => {
   test('renders footer', () => {
