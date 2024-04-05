7b52e8973f4df3de76629693a5b9d9501cb245c4
Andrew Monson <andrewmonsondev@gmail.com>
2024-04-05 09:51:47 -0400

    removed null | undefined types on rating interface

--- frontend/src/components/School.tsx
+++ frontend/src/components/School.tsx
@@ -1,17 +1,17 @@
 import { Card, CardBody, CardFooter, Button } from "@material-tailwind/react";
 import { Link } from "react-router-dom";
-import { Rating } from "./Rating";
+import { Rating } from "./";
 
 interface SchoolProps {
-  id?: string | null | undefined;
-  name?: string | null | undefined;
-  address?: string | null | undefined;
-  city?: string | null | undefined;
-  state?: string | null | undefined;
-  zipcode?: string | null | undefined;
-  phone?: string | null | undefined;
-  rating?: number | null | undefined;
-  max_tuition?: number | null | undefined;
+  id?: string;
+  name?: string;
+  address?: string;
+  city?: string;
+  state?: string;
+  zipcode?: string;
+  phone?: string;
+  rating?: number;
+  max_tuition?: number;
 }
 
 export const School = ({ school }: { school: SchoolProps }) => {
@@ -19,20 +19,20 @@ export const School = ({ school }: { school: SchoolProps }) => {
     <Card className="w-100 h-full my-4 mx-4">
       <CardBody className="flex flex-col p-4">
         <div>
-          <div className="text-lg font-bold">{school?.name}</div>
+          <div className="text-lg font-bold">{school.name}</div>
         </div>
         <div className="mt-2 self-baseline">
-          <div>{school?.address}</div>
+          <div>{school.address}</div>
           <div>
-            {school?.city}, {school?.state} {school?.zipcode}
+            {school.city}, {school?.state} {school?.zipcode}
           </div>
-          <div>{school?.phone}</div>
-          <div>{school?.rating && <Rating value={school?.rating} />}</div>
-          <div>{school?.max_tuition ?? 0 > 1000 ? "$$$$" : "$$$"}</div>
+          <div>{school.phone}</div>
+          <div>{school.rating && <Rating value={school?.rating} />}</div>
+          <div>{school.max_tuition ?? 0 > 1000 ? "$$$$" : "$$$"}</div>
         </div>
       </CardBody>
       <CardFooter>
-        <Link to={`/schools/${school?.id}`}>
+        <Link to={`/schools/${school.id}`}>
           <Button color="indigo" ripple={true}>
             Visit School Page
           </Button>
