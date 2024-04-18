e9093fb4ce0b695f228f3d85360a7d36be59a92b
Andrew Monson <andrewmonsondev@gmail.com>
2024-04-18 10:36:42 -0400

    reconfigured codegen for backend. was previously set up the same as the client

--- codegen.ts
+++ codegen.ts
@@ -1,33 +1,13 @@
-import { CodegenConfig } from "@graphql-codegen/cli";
+import type { CodegenConfig } from "@graphql-codegen/cli";
 
 const config: CodegenConfig = {
-  config: {
-    scalars: {
-      ObjectId: "ObjectId",
-      Void: "void",
-    },
-  },
+  overwrite: true,
   schema: "http://localhost:3005/graphql",
-  // this assumes that all your source files are in a top-level `src/` directory - you might need to adjust this to your file structure
-  documents: ["./backend/**/*.{ts,tsx}"],
   generates: {
-    "backend/__generatedTypes__/": {
-      preset: "client",
-      plugins: ["typescript-resolvers", "typescript-mongodb"],
-      config: {
-        useIndexSignature: true,
-        mappers: {
-          User: "./backend/models#userSchema",
-          School: "./backend/models#schoolsSchema",
-          Image: "./backend/models#imageSchema",
-        },
-      },
-      presetConfig: {
-        gqlTagName: "gql",
-      },
+    "backend/__generatedTypes__/graphql.ts": {
+      plugins: ["typescript", "typescript-resolvers", "typescript-mongodb"],
     },
   },
-  ignoreNoDocuments: true,
 };
 
 export default config;
