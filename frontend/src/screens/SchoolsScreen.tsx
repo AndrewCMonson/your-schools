711825e91b5450badf4e4f26d85e0b9e76f967d0
Andrew Monson <andrewmonsondev@gmail.com>
2024-04-26 09:40:04 -0400

    added useEffect to schoolsScreen that allows searches from other components to update search state

--- frontend/src/screens/SchoolsScreen.tsx
+++ frontend/src/screens/SchoolsScreen.tsx
@@ -1,4 +1,4 @@
-import { ReactElement } from "react";
+import { ReactElement, useEffect } from "react";
 import { useState } from "react";
 import { useSearchParams } from "react-router-dom";
 import { School, PageTitle, SearchBar } from "../components";
@@ -6,15 +6,22 @@ import { useGetSchools } from "../hooks/useGetSchools";
 import { LoadingScreen } from ".";
 import { School as SchoolType } from "../__generatedTypes__/graphql";
 import { LocationButton } from "../components/LocationButton";
+import { useSessionStore } from "../stores";
 
 export const SchoolsScreen = (): ReactElement => {
   const [searchParams, setSearchParams] = useSearchParams();
+  const { user } = useSessionStore();
   const [zipcode, setZipcode] = useState<string>(
-    searchParams.get("zipcode") || "",
+    searchParams.get("zipcode") || user?.zipcode || "",
   );
   const [search, setSearch] = useState<boolean>(false);
-
   const { loading, error, data: schools } = useGetSchools(zipcode);
+
+  useEffect(() => {
+    if (zipcode) {
+      setSearch(true);
+    }
+  }, [zipcode]);
 
   if (loading)
     return (
@@ -42,9 +49,10 @@ export const SchoolsScreen = (): ReactElement => {
             setSearchParams={setSearchParams}
           />
         </div>
-
         {schools.length === 0 && search ? (
-          <div className="text-center text-2xl mt-8">No schools found</div>
+          <div className="text-center text-2xl mt-8">
+            No schools found in {zipcode}
+          </div>
         ) : (
           <div className="flex flex-col min-w-1/2 ">
             {schools.map((school) => (
